c4c10a7ba0339120b3f22ea4034fc431d6286c9e
au-aii <tanakaharuk127@gmail.com>
2025-07-22 09:52:25 +0900

    Delete redundant setup data

--- setup_nltk.py
+++ setup_nltk.py
@@ -2,6 +2,4 @@
 # Run it only once during initial setup.
 import nltk
 nltk.download('cmudict')
-nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
 print("NLTK data downloaded successfully!")
