f76a75b7c4a1bcc607e8c2dbad3bfdaf90bb1aae
au-aii <tanakaharuk127@gmail.com>
2025-07-29 22:37:52 +0900

    style : remove port comment

--- web/app.py
+++ web/app.py
@@ -214,4 +214,4 @@ def static_files(filename):
     return send_from_directory('static', filename)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)  # ポートを5000から8080に変更
+    app.run(debug=True, host='0.0.0.0', port=8080)  
